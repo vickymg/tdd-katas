75d365070100e7a2767f358b0023181bb5d6c3ce
Vicky Gray <vickygray26@hotmail.com>
2016-06-05 16:53:56 +0100

    returns a message when passed zero

--- roman_numerals/spec/numerals_coverter_spec.rb
+++ roman_numerals/spec/numerals_coverter_spec.rb
@@ -6,35 +6,4 @@ describe "#numerals_coverter" do
     expect(numerals_converter(0)).to eq("Romans didn't have zero!")
   end
 
-  it "should return I when passed 1" do
-    expect(numerals_converter(1)).to eq("I")
-  end
-
-  it "should return II when passed 2" do
-    expect(numerals_converter(2)).to eq("II")
-  end
-
-  it "should return IV when passed 4" do
-    expect(numerals_converter(4)).to eq("IV")
-  end
-
-  it "should return V when passed 5" do
-    expect(numerals_converter(5)).to eq("V")
-  end
-
-  it "should return IX when passed 9" do
-    expect(numerals_converter(9)).to eq("IX")
-  end
-
-  it "should return X when passed 10" do
-    expect(numerals_converter(10)).to eq("X")
-  end
-
-  it "should return XXXV when passed 35" do
-    expect(numerals_converter(35)).to eq("XXXV")
-  end
-
-  it "should return CMXCVI when passed 996" do
-    expect(numerals_converter(996)).to eq("CMXCVI")
-  end
 end
