f47b23cbce5474ac071360173815685f666488bb
Vicky Gray <vickygray26@hotmail.com>
2016-06-06 20:59:10 +0100

    it returns IV when passed 4 and IX when passed 9

--- roman_numerals/lib/numerals_coverter.rb
+++ roman_numerals/lib/numerals_coverter.rb
@@ -1,6 +1,8 @@
 def numerals_converter(num)
   numerals = { 10 => "X",
+               9 => "IX",
                5 => "V",
+               4 => "IV",
                1 => "I"
              }
   return "Romans didn't have zero!" if num == 0
