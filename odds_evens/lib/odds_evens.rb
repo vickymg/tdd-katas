0314a28037e32b2201a1d3876a366931cd33889f
Vicky Gray <vickygray26@hotmail.com>
2016-06-14 14:06:57 +0100

    returns number if prime

--- odds_evens/lib/odds_evens.rb
+++ odds_evens/lib/odds_evens.rb
@@ -1,4 +1,7 @@
+require 'prime'
+
 def oddsEvens(num)
+  return num if Prime.prime?(num)
   return "Odd" if num.odd?
   return "Even" if num.even?
 end
--- odds_evens/spec/odds_evens_spec.rb
+++ odds_evens/spec/odds_evens_spec.rb
@@ -3,11 +3,15 @@ require 'odds_evens'
 describe 'oddsEvens' do
 
   it 'should return odd if passed an odd number' do
-    expect(oddsEvens(3)).to eq('Odd')
+    expect(oddsEvens(1)).to eq('Odd')
   end
 
   it 'should return even if passed an even number' do
-    expect(oddsEvens(2)).to eq("Even")
+    expect(oddsEvens(6)).to eq("Even")
+  end
+
+  it 'should return the number if it is prime' do
+    expect(oddsEvens(3)).to eq(3)
   end
 
 end
