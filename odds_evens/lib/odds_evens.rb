0bbe7fa2842fbc4a69de611fea57ca722c3f548a
Vicky Gray <vickygray26@hotmail.com>
2016-06-14 13:52:14 +0100

    returns 'odd' when passed an odd number

--- odds_evens/lib/odds_evens.rb
+++ odds_evens/lib/odds_evens.rb
@@ -1,2 +1,3 @@
-#Write a program that prints numbers within specified range lets say 1 to 100. If number is odd print 'Odd'
-#instead of the number. If number is even print 'Even' instead of number. Else print number [hint - if number is Prime].
+def oddsEvens(num)
+  "Odd"
+end
--- odds_evens/spec/odds_evens_spec.rb
+++ odds_evens/spec/odds_evens_spec.rb
@@ -1,7 +1,9 @@
-# Lets divide into following steps:
-# - Prints numbers from 1 to 100
-# - Print "Even" instead of number, if the number is even, means divisible by 2
-# - Print "Odd" instead of number, if the number is odd, means not divisible by 2 but not divisible by itself too [hint - it should not be Prime]
-# - Print number, if it does not meet above two conditions, means if number is Prime
-# - Make method to accept any number of range [currently  we have 1 to 100]
-# - Create a new method to check Odd/Even/Prime of a single supplied method
+require 'odds_evens'
+
+describe 'oddsEvens' do
+
+  it 'should return odd if passed an odd number' do
+    expect(oddsEvens(3)).to eq('Odd')
+  end
+
+end
