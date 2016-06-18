9d8aaf34705fa8a8310f871120429a11415f9377
Vicky Gray <vickygray26@hotmail.com>
2016-06-18 17:24:29 +0100

    returns X when passed 10

--- roman_numerals/lib/numerals_coverter.rb
+++ roman_numerals/lib/numerals_coverter.rb
@@ -1,7 +1,8 @@
 def numerals_converter(num)
   numerals = {
     1 => "I",
-    5 => "V"
+    5 => "V",
+    10 => "X"
   }
   return "Not a valid number for conversion!" if num <= 0
   numerals[num]
