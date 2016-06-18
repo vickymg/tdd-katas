140902f82380dab06089b3d35e1e6a6192e37cf3
Vicky Gray <vickygray26@hotmail.com>
2016-06-18 16:22:13 +0100

    reset to first test

--- roman_numerals/lib/numerals_coverter.rb
+++ roman_numerals/lib/numerals_coverter.rb
@@ -1,17 +1,3 @@
 def numerals_converter(num)
-  numerals = { 10 => "X",
-               9 => "IX",
-               5 => "V",
-               4 => "IV",
-               1 => "I"
-             }
-  return "Romans didn't have zero!" if num == 0
-  result = ""
-  numerals.each do |arabic, roman|
-    (num / arabic).times do
-      result << roman
-      num -= arabic
-    end
-  end
-  result
+
 end
--- roman_numerals/spec/numerals_coverter_spec.rb
+++ roman_numerals/spec/numerals_coverter_spec.rb
@@ -6,32 +6,4 @@ describe "#numerals_coverter" do
     expect(numerals_converter(0)).to eq("Romans didn't have zero!")
   end
 
-  it "should return I when passed 1" do
-    expect(numerals_converter(1)).to eq("I")
-  end
-
-  it "should return II when passed 2" do
-    expect(numerals_converter(2)).to eq("II")
-  end
-
-  it "should return IV when passed 4" do
-    expect(numerals_converter(4)).to eq("IV")
-  end
-
-  it "should return V when passed 5" do
-    expect(numerals_converter(5)).to eq("V")
-  end
-
-  it "should retun VI when passed 6" do
-    expect(numerals_converter(6)).to eq("VI")
-  end
-
-  it "should return IX when passed 9" do
-    expect(numerals_converter(9)).to eq("IX")
-  end
-
-  it "should return X when passed 10" do
-    expect(numerals_converter(10)).to eq("X")
-  end
-
 end
