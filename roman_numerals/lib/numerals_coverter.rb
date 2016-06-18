21bf27d66762197929ee217a4aed18f7ecdfb32c
Vicky Gray <vickygray26@hotmail.com>
2016-06-18 17:19:54 +0100

    returns a message if passed 0

--- roman_numerals/lib/numerals_coverter.rb
+++ roman_numerals/lib/numerals_coverter.rb
@@ -1,3 +1,3 @@
 def numerals_converter(num)
-
+  return "Not a valid number for conversion!" if num <= 0
 end
--- roman_numerals/spec/numerals_coverter_spec.rb
+++ roman_numerals/spec/numerals_coverter_spec.rb
@@ -3,7 +3,7 @@ require 'numerals_coverter'
 describe "#numerals_coverter" do
 
   it "should return a message if number is zero" do
-    expect(numerals_converter(0)).to eq("Romans didn't have zero!")
+    expect(numerals_converter(0)).to eq("Not a valid number for conversion!")
   end
 
 end
